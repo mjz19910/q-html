70df2836e9e274b9a1d702967e5888b33fb182df
mjz19910 <mjz19910@users.noreply.github.com>
2021-08-23 20:01:30 -0700

    lex:Add loop to parse all lex_tokens

--- lex_q.js
+++ lex_q.js
@@ -29,40 +29,43 @@ function panic(str) {
 function lex(m_str) {
 	let index = 0;
 	let lex_tok_arr = [];
-	let c = m_str[index];
-	switch (c) {
-		case '()'[0]: {
-			lex_tok_arr.push(new LPara(c));
-			break;
-		}
-		case '()'[1]: {
-			lex_tok_arr.push(new RPara(c));
-			break;
-		}
-		case '+': {
-			lex_tok_arr.push(new AddToken(c));
-			break;
-		}
-		case '0': {
-			let lit = new HexLiteral;
-			if (m_str[index + 1] != 'x') {
-				panic('lex error');
+	fl: for (; index < m_str.length;) {
+		let c = m_str[index];
+		switch (c) {
+			case '()'[0]: {
+				lex_tok_arr.push(new LPara(c));
+				break;
 			}
-			let acc_str = '';
-			acc_str += '0x';
-			let ci = index + 2;
-			let cc = m_str[ci];
-			for (; ci<m_str.length&&cc.match(/[a-fA-F0-9]/); ci++) {
-				cc = m_str[ci];
-				acc_str += cc;
+			case '()'[1]: {
+				lex_tok_arr.push(new RPara(c));
+				break;
 			}
-			lit.set_lit_text(acc_str);
-			lex_tok_arr.push(lit);
-			break;
+			case '+': {
+				lex_tok_arr.push(new AddToken(c));
+				break;
+			}
+			case '0': {
+				let lit = new HexLiteral;
+				if (m_str[index + 1] != 'x') {
+					panic('lex error');
+				}
+				let acc_str = '';
+				acc_str += '0x';
+				let ci = index + 2;
+				let cc = m_str[ci];
+				for (; ci < m_str.length && cc.match(/[a-fA-F0-9]/); ci++) {
+					cc = m_str[ci];
+					acc_str += cc;
+				}
+				lit.set_lit_text(acc_str);
+				lex_tok_arr.push(lit);
+				break;
+			}
+			default:
+				if (c.charCodeAt(0) > 20) { };
+				console.log('bad char in lex', c, c.charCodeAt(0));
+				break fl;
 		}
-		default:
-			if (c.charCodeAt(0) > 20) { };
-			console.log('bad char in lex', c, c.charCodeAt(0));
 	}
 	return lex_tok_arr;
 }
